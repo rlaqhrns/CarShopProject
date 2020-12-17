57273210b8acaddb106846be297428d5c7ac21a0
Emma baby <bomvll@naver.com>
2020-12-17 16:04:12 +0900

    commit

--- src/main/java/com/shop/controller/HomeController.java
+++ src/main/java/com/shop/controller/HomeController.java
@@ -4,7 +4,10 @@ package com.shop.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.protobuf.Service;
 
 /**
  * Handles requests for the application home page.
@@ -49,8 +52,9 @@ public class HomeController {
 	}
 	
 	//보미_로그인성공시 index화면으로 돌아가기(로그인된상태_로그인&회원가입 버튼 안보이게)
-	@RequestMapping("/index")
-	public String index2() {
+	@PostMapping("/login")
+	public String login_success(All_User_Tbl all_User_Tbl) {
+		//service.idpw_check(all_User_Tbl); //db로부터 id.pw가져오기
 		return "redirect:/carshop/index";       //세션객체 생성해서 처리할 것 
 	}
 	
