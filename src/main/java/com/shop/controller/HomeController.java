7e26891b2c0434ca4df6ac427a0ab1f860beff40
ssw74 <ssw74@192.168.219.104>
2020-12-18 10:13:23 +0900

    1

--- src/main/java/com/shop/controller/HomeController.java
+++ src/main/java/com/shop/controller/HomeController.java
@@ -5,18 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.shop.service.CarShopService;
-
+import com.shop.vo.Prod_Tbl;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 @RequestMapping("/carshop/*")
-
+@Slf4j
 public class HomeController {
 
 	@Setter(onMethod_ = @Autowired)
@@ -28,10 +30,10 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 
-	@RequestMapping({ "/", "index" })
+	@RequestMapping("index" )
 	public String index(Model model) {
 		model.addAttribute("list", service.index());
-
+		model.addAttribute("list2",service.index2());
 		return "carshop/index";
 
 	}
@@ -47,9 +49,20 @@ public class HomeController {
 		return "carshop/checkout";
 	}
 
-	@RequestMapping("/home")
-	public String a() {
-		return "home";
+	
+	@GetMapping("/productForm")
+	public String productForm(Model model) {
+		System.out.println("productForm 들어옴");
+		
+		return "carshop/productForm";
+	}
+	
+	// action 에서 post 방식으로 들어올 시 입력 폼
+	@PostMapping("/productForm")
+	public String register(Prod_Tbl product) {
+		log.info("컨틀롤러에서 등록 " +  product);
+		service.productForm(product);
+		return "redirect:/carshop/index";
 	}
 
 }
