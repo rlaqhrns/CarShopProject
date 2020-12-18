0e6492ce981fc265a858e7006e047da702cb5348
Emma baby bomvll@naver.com <Emma baby bomvll@naver.com>
2020-12-18 17:29:50 +0900

    commit

--- src/main/java/com/shop/service/CarShopService.java
+++ src/main/java/com/shop/service/CarShopService.java
@@ -2,10 +2,12 @@ package com.shop.service;
 
 import java.util.List;
 
+import com.shop.vo.All_User_Tbl;
 import com.shop.vo.Prod_Tbl;
 
 public interface CarShopService {
 	
-	public List<Prod_Tbl> index();
+//	public List<Prod_Tbl> index();
+//	public All_User_Tbl getAllUser();
 
 }
--- src/main/java/com/shop/service/CarShopServiceImple.java
+++ src/main/java/com/shop/service/CarShopServiceImple.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shop.mapper.CarShopMapper;
+import com.shop.vo.All_User_Tbl;
 import com.shop.vo.Prod_Tbl;
 
 import lombok.Setter;
@@ -13,13 +14,24 @@ import lombok.Setter;
 @Service
 public class CarShopServiceImple implements CarShopService {
 
-	@Setter(onMethod_ = @Autowired)
-	private CarShopMapper mapper;
-
-	@Override
-	public List<Prod_Tbl> index() {
-		System.out.println("mapper list 들어옴");
-		return mapper.index();
-	}
+//	@Setter(onMethod_ = @Autowired)
+//	private CarShopMapper mapper;
+//
+//	@Override
+//	public List<Prod_Tbl> index() {
+//		System.out.println("mapper list 들어옴");
+//		return mapper.index();
+//	}
+//
+//	@Override
+//	public All_User_Tbl getAllUser() {
+//		System.out.println("mapper 회원로그인정보 가져옴");
+//		return mapper.getAllUser();
+//	}
+//	
+//	
+	
+	
+	
 
 }
