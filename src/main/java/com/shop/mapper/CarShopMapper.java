1ca40b2135c3e1c6dab8ae13e2c266934d3b4cce
Emma baby bomvll@naver.com <Emma baby bomvll@naver.com>
2020-12-18 17:29:41 +0900

    commit

--- src/main/java/com/shop/mapper/CarShopMapper.java
+++ src/main/java/com/shop/mapper/CarShopMapper.java
@@ -2,10 +2,12 @@ package com.shop.mapper;
 
 import java.util.List;
 
+import com.shop.vo.All_User_Tbl;
 import com.shop.vo.Prod_Tbl;
 
 public interface CarShopMapper {
 
 	public List<Prod_Tbl> index();
+	public All_User_Tbl getAllUser();
 
 }
