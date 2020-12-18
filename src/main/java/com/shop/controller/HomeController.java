527748830a72553fba1284e7145917209e50304f
ssw74 <ssw74@192.168.219.104>
2020-12-18 11:59:05 +0900

    1

--- src/main/java/com/shop/controller/HomeController.java
+++ src/main/java/com/shop/controller/HomeController.java
@@ -5,18 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shop.service.CarShopService;
+import com.shop.vo.Prod_Tbl;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 @RequestMapping("/carshop/*")
-
+@Slf4j
 public class HomeController {
 
 	@Setter(onMethod_ = @Autowired)
@@ -51,18 +55,18 @@ public class HomeController {
 	public String a() {
 		return "home";
 	}
-	
+
 	@GetMapping("/productForm")
 	public String productForm(Model model) {
 		System.out.println("productForm 들어옴");
-		
+		model.addAttribute("category", service.category());
 		return "carshop/productForm";
 	}
-	
+
 	// action 에서 post 방식으로 들어올 시 입력 폼
 	@PostMapping("/productForm")
 	public String register(Prod_Tbl product) {
-		log.info("컨틀롤러에서 등록 " +  product);
+		log.info("컨틀롤러에서 등록 " + product);
 		service.productForm(product);
 		return "redirect:/carshop/index";
 	}
