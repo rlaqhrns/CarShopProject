53f09fe8771de4287b44614d94051669b86486b8
rlaqhrns <63628096+rlaqhrns@users.noreply.github.com>
2020-12-23 18:22:47 +0900

    리스트 찜하기 기능, 장바구니기능 완성 controller: 	ProductBuy mapper: 	likeMapper service: 	LikeService jsp: 	productList img: 	headLampe 	heart

--- src/main/java/com/shop/service/product/LikeService.java
+++ src/main/java/com/shop/service/product/LikeService.java
@@ -1,5 +1,14 @@
 package com.shop.service.product;
 
-public interface LikeService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.shop.vo.Like_Tbl;
+
+@Service
+public interface LikeService {
+	public int addLike(Like_Tbl like);
+	public List<Like_Tbl> getUserLikedList(String u_id);
+	public int deleteLike(int p_no, String u_id);
 }
