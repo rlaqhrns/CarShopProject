036d51599dcc869a96167cad3025eb66f04d3c43
findm <findm@DESKTOP-HDO3D2F>
2020-12-29 09:55:18 +0900

    # WARNING: head commit changed in the meantime
    commit

--- src/main/java/com/shop/vo/Cart_Tbl.java
+++ src/main/java/com/shop/vo/Cart_Tbl.java
@@ -5,15 +5,15 @@ import lombok.Data;
 @Data
 public class Cart_Tbl {
 	
-	private String u_id,pname;
-	private int pno,quantity,amount,total;
+	private String u_id,pname, cart_id;
+	private int p_no,quantity,amount,total;
 	// amount 총금액
 
 	
 	
 	  @Override
 	    public String toString() {
-	        return "Cart_Tbl [u_id=" + u_id + ", pname=" + pname + ", pno=" + pno + ", quantity=" + quantity
+	        return "Cart_Tbl [u_id=" + u_id + ",cart_id" + cart_id+ ", pname=" + pname + ", p_no=" + p_no + ", quantity=" + quantity
 	                + ", amount=" + amount  + "]";
 	    }
 	  
