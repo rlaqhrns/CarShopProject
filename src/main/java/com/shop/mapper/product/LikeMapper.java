4ee55351d430e561aedac99013ea6f40bbab2364
rlaqhrns <63628096+rlaqhrns@users.noreply.github.com>
2020-12-29 09:50:03 +0900

    리스트 찜취소 기능, 카테고리, 상세카테고리, 카테고리로 리스트 불러온후 이벤트 버그 수정 join vo 생성 	ProductCategoryJoin.java controller: 	ProductBuyController.java mapper: 	likeMapper 	productbuy service: 	LikeService 	productbuy jsp: 	productList vo: 	prod_tbl(공백);;

--- src/main/java/com/shop/mapper/product/LikeMapper.java
+++ src/main/java/com/shop/mapper/product/LikeMapper.java
@@ -2,10 +2,13 @@ package com.shop.mapper.product;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.shop.vo.Like_Tbl;
 
 public interface LikeMapper {
 	public int addLike(Like_Tbl like);
 	public List<Like_Tbl> getUserLikedList(String u_id);
-	public int deleteLike(int p_no, String u_id);
+	public Like_Tbl getUserLikeProduct(@Param("p_no")int p_no,@Param("u_id")String u_id);
+	public int deleteLike(@Param("p_no")int p_no,@Param("u_id")String u_id);
 }
