7ab6efd29f7bcd37f3ce96444397eda46e46a916
findm <findm@DESKTOP-HDO3D2F>
2020-12-29 14:14:29 +0900

    user tbl 추가

--- src/main/java/com/shop/vo/User_Tbl.java
+++ src/main/java/com/shop/vo/User_Tbl.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class User_Tbl {
-	private String u_id, u_pw, name, phone, juso1,juso2,juso3, cars, email;
+	private String u_id, u_pw, name, phone, addr,addr2, cars, email;
 	// hyejeong 2020.12.22 table name change
+	// hyejeong 2020.12.29 table 칼럼 addr, addr2 추가
 
 }
