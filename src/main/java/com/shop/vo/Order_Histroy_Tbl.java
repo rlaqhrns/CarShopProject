21c088476d617bc2dd184bdff4ea56c61b3034c4
JaewonP <protectingjw@naver.com>
2020-12-29 10:13:30 +0900

    commit

--- src/main/java/com/shop/vo/Order_Histroy_Tbl.java
+++ src/main/java/com/shop/vo/Order_Histroy_Tbl.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Order_Histroy_Tbl {
-	private String u_id, pay, order_date;
+	private String u_id, pay, order_date, pname;
 	private int ono, pno, quantity, amount;
 
 }
